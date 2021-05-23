d8cad38cad0023c6c1d041cace73bd75143ee39a
Parker Jones <parker.emailaddress@sharpspring.com>
2021-05-23 10:49:35 -0400

    spec out the project heirarchy

---
+++ src/bus/mod.rs
@@ -0,0 +1,13 @@
+mod bus;
+
+pub struct Bus {
+    cpu_vram: [u8; 2048]
+ }
+ 
+ impl Bus {
+    pub fn new() -> Self{
+        Bus {
+            cpu_vram: [0; 2048]
+        }
+    }
+ }
---
+++ src/ppu/mod.rs
@@ -0,0 +1,10 @@
+mod ppu;
+
+pub struct PPU {
+    pub chr_rom: Vec<u8>,
+    pub palette_table: [u8; 32],
+    pub vram: [u8; 2048],
+    pub oam_data: [u8; 256],
+ 
+    pub mirroring: Mirroring,
+ }
