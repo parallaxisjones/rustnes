d89585d33a4ee42f711267697f1789e1d6aaf6f2
Parker Jones <parker.emailaddress@sharpspring.com>
2021-05-23 16:47:33 -0400

    AdressingMode started

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -11,18 +11,37 @@ pub struct CPU {
     pub status: u8,
     // Program Counter Register
     pub program_counter: u16,
+    memory: [u8; 0xFFFF]
 }
 
 #[derive(FromPrimitive)]
 pub enum OpsCode {
     // Load Accumulator
-    LDA = 0xA9,
+    LDA = 0xa9,
+    
     // Break
     BRK = 0x00,
+
     // Transfer Accumulator to X
     TAX = 0xAA,
+
     // Increment X Register
     INX = 0xE8
+}
+
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub enum AddressingMode {
+   Immediate,
+   ZeroPage,
+   ZeroPage_X,
+   ZeroPage_Y,
+   Absolute,
+   Absolute_X,
+   Absolute_Y,
+   Indirect_X,
+   Indirect_Y,
+   NoneAddressing,
 }
 
 impl CPU {
@@ -32,6 +51,8 @@ impl CPU {
             register_x: 0,
             status: 0,
             program_counter: 0,
+            // We have just created an array for the whole 64 KiB of address space
+            memory: [0; 0xFFFF]
         }
     }
 
@@ -54,6 +75,54 @@ impl CPU {
                 None => println!("unknown OpCode {}", opscode),
             }
         }
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run()
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+ 
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        // We load program code into memory, starting at 0x8000 address.
+        // [0x8000 .. 0xFFFF] is reserved for Program ROM, 
+        // and we can assume that the instructions stream should start somewhere in this space (not necessarily at 0x8000).
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.mem_write_u16(0xFFFC, 0x8000);
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let opscode = self.mem_read(self.program_counter);
+            self.program_counter += 1;
+
+            match FromPrimitive::from_u8(opscode) {
+                Some(OpsCode::LDA) => {
+                    let param = self.mem_read(self.program_counter);
+                    self.program_counter += 1;
+                    self.lda(param);
+                },
+                Some(OpsCode::TAX) => self.tax(),
+                Some(OpsCode::INX) => self.inx(),
+                Some(OpsCode::BRK) => return,
+                None => println!("unknown OpCode {}", opscode),
+            }
+        }
+    }
+
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data
     }
     fn lda(&mut self, value: u8) {
         self.register_a = value;
@@ -85,6 +154,17 @@ impl CPU {
         } else {
             self.status = self.status & 0b0111_1111;
         }
+    }
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos + 1) as u16;
+        (hi << 8) | (lo as u16)
     }
 }
 
